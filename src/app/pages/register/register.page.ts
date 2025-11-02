535d13c0268dd225a229eb2929b2bc074aa9c3ce
Juan-Moren <juan.morenobaquero@unicolombo.edu.co>
2025-11-02 14:06:53 -0500

    Creation of home page, register page and minor style changes

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,16 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
-  standalone: false,
+  standalone: false
 })
 export class RegisterPage implements OnInit {
+  registerForm!: FormGroup;
+  logoExists: boolean = true;
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit() {
+    this.registerForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      confirmPassword: new FormControl('', Validators.required)
+    });
   }
 
+  onLogoError() {
+    this.logoExists = false;
+  }
+
+  onLogoLoad() {
+    this.logoExists = true;
+  }
+
+  onSubmit() {
+    if (this.registerForm.invalid) return;
+
+    const { password, confirmPassword } = this.registerForm.value;
+    if (password !== confirmPassword) {
+      alert('Passwords do not match');
+      return;
+    }
+
+    // Here you can later implement real registration logic
+    console.log('Registered user:', this.registerForm.value);
+    this.router.navigate(['/login']);
+  }
+
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
 }
